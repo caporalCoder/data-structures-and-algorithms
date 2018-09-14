300d6ebc1dd77f90eec2160e473000f05a2f876d
mxt111 <foromodanielsoromou@gmail.com>
2018-09-13 23:06:21 -0400

    A multiplication table

---
+++ codeforces/A_multiplication_table.cpp
@@ -0,0 +1,66 @@
+/*
+ * autoor : mxt111
+ */
+ 
+#include <vector>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <algorithm>
+#include <unordered_map>
+#include <set>
+#include <cmath>
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <chrono>
+#include <complex>
+
+
+#define distance(a, b) abs(a - b)
+#define endl "\n"
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define f first
+#define s second
+
+
+using namespace std;
+
+// g++ --std=c++14 -o x.o x.cpp
+
+int main() {
+	
+	std::ios::sync_with_stdio(false);
+	int n, k;
+	cin >> n >> k;
+
+	int cpt = 0;
+
+	int i ;
+
+	i = (n >= k) ? 1 : 2;
+
+	for(; i <= n; ++i) {
+		int x = k / i;
+
+		if (x <= n && (x * i) == k) {
+			++cpt;
+		}
+	}
+
+	cout << cpt;
+	return 0;
+}
