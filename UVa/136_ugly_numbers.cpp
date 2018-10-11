3ccdb8814dea2327a598d5c43edb76928523a51f
Daniel Soromou <danielsoromou02@gmail.com>
2018-10-11 16:21:33 -0400

    136_ugly_numbers.cpp

--- UVa/136_ugly_numbers.cpp
+++ UVa/136_ugly_numbers.cpp
@@ -24,26 +24,26 @@ long long uglyRankNumber(long long n) {
 	unsigned next_multiple_of_2 = 2; 
 	unsigned next_multiple_of_3 = 3; 
 	unsigned next_multiple_of_5 = 5; 
-	unsigned next_ugly_no = 1; 
+	unsigned next_ugly = 1; 
 
 	ugly[0] = 1; 
 	for (int i = 1; i < n; i++) { 
-		next_ugly_no = min(next_multiple_of_2, min(next_multiple_of_3, next_multiple_of_5)); 
-		ugly[i] = next_ugly_no; 
-		if (next_ugly_no == next_multiple_of_2) { 
+		next_ugly = min(next_multiple_of_2, min(next_multiple_of_3, next_multiple_of_5)); 
+		ugly[i] = next_ugly; 
+		if (next_ugly == next_multiple_of_2) { 
 			i2 += 1;
 			next_multiple_of_2 = ugly[i2]*2; 
 		} 
-		if (next_ugly_no == next_multiple_of_3) { 
+		if (next_ugly == next_multiple_of_3) { 
 			i3 += 1;
 			next_multiple_of_3 = ugly[i3]*3; 
 		} 
-		if (next_ugly_no == next_multiple_of_5) { 
+		if (next_ugly == next_multiple_of_5) { 
 			i5 += 1;
 			next_multiple_of_5 = ugly[i5]*5; 
 		} 
 	} 
-	return next_ugly_no; 
+	return next_ugly; 
 } 
 
 int main() {
