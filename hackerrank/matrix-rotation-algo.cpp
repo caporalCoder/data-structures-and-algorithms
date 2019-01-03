f531f8c9ca7f174bd5bca4c49753d2a1d7948ed8
Daniel Soromou <danielsoromou02@gmail.com>
2019-01-03 01:12:35 -0500

    Matrix rotation algo

--- hackerrank/matrix-rotation-algo.cpp
+++ hackerrank/matrix-rotation-algo.cpp
@@ -32,6 +32,47 @@
 using namespace std;
 
 int main() {
-	
+	ll n, m, r;
+    cin >> n >> m >> r;
+    vector<vector<ll> > grid(n, vector<ll>(m));
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < m; ++j) {
+            cin >> grid[i][j];
+        }
+    }
+
+    ll k = min(n, m) / 2;
+
+
+    for(int i = 0; i < k; ++i) {
+        vector<ll> circonference;
+        for(int j = i; j < m - i - 1; ++j) circonference.push_back(grid[i][j]); 
+        for(int j = i; j <= n - i - 1; ++j) circonference.push_back(grid[j][m - i - 1]);
+        for(int j = m - i - 2; j >= i; --j) circonference.push_back(grid[n - i - 1][j]);
+        for(int j = n - i - 2; j >= i + 1; --j) circonference.push_back(grid[j][i]);
+
+        int g = (int) circonference.size();
+        vector<ll> rCirconference(g);
+        int r_k = r % g;
+       
+        for(int h = 0; h < g; ++h) {
+            rCirconference[(h -  r_k + g) % g] = circonference[h];
+        }
+  
+        int x = 0;
+        for(int j = i; j < m - i - 1; ++j) grid[i][j] = rCirconference[x++];
+        for(int j = i; j <= n - i - 1; ++j) grid[j][m - i - 1] = rCirconference[x++];
+        for(int j = m - i - 2; j >= i; --j) grid[n - i - 1][j] = rCirconference[x++];
+        for(int j = n - i - 2; j >= i + 1; --j) grid[j][i] = rCirconference[x++];
+
+    }
+
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < m; ++j) {
+            cout <<  grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+
 	return 0;
 }
