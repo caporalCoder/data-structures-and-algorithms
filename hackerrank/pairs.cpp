b997b138c5612b359bc54ca938cc90785575088e
Foromo Daniel Soromou <foromo_daniel.soromou@bell.ca>
2019-03-15 09:13:08 -0400

    Pairs 100% unordered_set

---
+++ hackerrank/pairs.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <list>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <complex>
+#define endl "\n"
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+
+	
+using namespace std;
+
+int main() {
+	unsigned long long n, k;
+    cin >> n >> k;
+    
+    vector<unsigned long long> nums(n);
+
+    for(int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+
+    sort(nums.begin(), nums.end());
+    int cpt = 0; 
+    for(int i = 0; i < n - 1; ++i) {
+        for(int j = i + 1; j < n; ++j) {
+            if ((nums[j] - nums[i]) == k) {
+                ++cpt;
+                break;
+            }
+        }
+    }
+
+    cout << cpt << endl;
+
+	return 0;
+}
