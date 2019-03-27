ac861d2c69bcde2b73974edd1b0b43eb81c4c748
Foromo Daniel Soromou <foromo_daniel.soromou@bell.ca>
2019-03-27 13:30:32 -0400

    Add template code

---
+++ usaco/template.cpp
@@ -0,0 +1,43 @@
+/*
+ID: fromod1
+PROG: numtri
+LANG: C++
+*/
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define endl "\n"
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define f first
+#define s second
+
+using namespace std;
+
+int main() {
+	ifstream fin;
+	ofstream fout;
+	fin.open("numtri.in");
+	fout.open("numtri.out");
+	
+	fout.close();
+	fin.close();
+	return 0;
+}
