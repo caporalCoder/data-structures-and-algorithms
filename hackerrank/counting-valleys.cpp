7f06e2149f9c8478fb9bb09a8deb0aef880c36ce
Foromo Daniel Soromou <foromo_daniel.soromou@bell.ca>
2019-07-05 13:26:13 -0400

    Add Couting valley and socket merchant

---
+++ hackerrank/counting-valleys.cpp
@@ -0,0 +1,61 @@
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <list>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <complex>
+
+
+#define endl "\n"
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+
+
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<char> data(n);
+    for(int i = 0; i < n; ++i) {
+        cin >> data[i];
+    }
+
+    int numberOfValey = 0, level = 0;
+    bool outOfValey = true;
+
+    for(char d: data) {
+        if (level < 0 && outOfValey) {
+            ++numberOfValey;
+            outOfValey = false;
+        } else if (level >= 0) {
+            outOfValey = true;
+        }
+
+        if (d=='U') ++level;
+        else --level;
+    }
+
+    cout << numberOfValey << endl;
+
+    return 0;
+}
