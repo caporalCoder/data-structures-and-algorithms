05a3ad131d23672423d45bae91e323df819c3567
Daniel Soromou <fosoromo@microsoft.com>
2023-08-21 14:58:49 -0700

    More doing

---
+++ hackerrank/Implementation/strange-code.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <list>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <complex>
+#define endl "\n"
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+
+	
+using namespace std;
+
+int main() {
+    ll t;
+    std::cin >> t;
+
+    ll phaseStart = 3;
+    ll elapsed = 3;
+    
+    while (t > elapsed) {
+        phaseStart *= 2;
+        elapsed += phaseStart;
+    }
+
+    std::cout << elapsed - t + 1 << endl;
+
+    return 0;
+}
