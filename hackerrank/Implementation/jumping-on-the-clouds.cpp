f5c8ef821e894f3aaa9e9fcaea4da95c66c813cb
Daniel Soromou <fosoromo@microsoft.com>
2023-08-21 15:16:44 -0700

    More coding

---
+++ hackerrank/Implementation/jumping-on-the-clouds.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <list>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <complex>
+#define endl "\n"
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+
+	
+using namespace std;
+
+int main() {
+    int n;
+    std::cin >> n;
+    vi c(n);
+
+    for (int i = 0; i < n; ++i) {
+        std::cin >> c[i];
+    }
+
+    int jumps = 0;
+    int i = 0;
+    while (i < n - 1) {
+        if (i + 2 < n && c[i + 2] == 0) {
+            i += 2;
+        } else {
+            ++i;
+        }
+        ++jumps;
+    }
+
+    std::cout << jumps << endl;
+
+    return 0;
+}
